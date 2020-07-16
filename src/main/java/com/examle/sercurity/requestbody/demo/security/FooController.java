db2831c26e130dcaac421f7e1c4e74f4dda264d7
Grzegorz Kapcia <grzegorzkapcia@gmail.com>
2020-07-16 15:52:09 +0200

    commit

--- src/main/java/com/examle/sercurity/requestbody/demo/security/FooController.java
+++ src/main/java/com/examle/sercurity/requestbody/demo/security/FooController.java
@@ -1,12 +1,13 @@
 package com.examle.sercurity.requestbody.demo.security;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FooController {
     @RequestMapping("/foo")
-    public String foo(Foo foo) {
+    public String foo(@RequestBody Foo foo) {
         return foo.getBar();
     }
 }
